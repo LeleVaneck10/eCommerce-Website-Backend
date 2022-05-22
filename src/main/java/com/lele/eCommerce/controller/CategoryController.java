1ddc14c29d5216f711b535a899d30ba88311c3da
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2022-05-22 01:54:59 +0100

    Created the controller package and setup the CategoryController class with the PostMapping annotation

---
+++ src/main/java/com/lele/eCommerce/controller/CategoryController.java
@@ -0,0 +1,24 @@
+package com.lele.eCommerce.controller;
+
+import com.lele.eCommerce.model.Category;
+import com.lele.eCommerce.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+
+    @Autowired
+    CategoryService categoryService;
+
+    @PostMapping("/create")
+    public String creatCategory(@RequestBody Category category) {
+
+        categoryService.createCategory(category);
+        return "success";
+    }
+}
