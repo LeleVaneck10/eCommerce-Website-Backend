abc8f2950de82190a42982d2197dee8d9113a1b1
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2022-06-01 04:47:20 +0100

    Modifications

--- src/main/java/com/lele/eCommerce/service/ProductService.java
+++ src/main/java/com/lele/eCommerce/service/ProductService.java
@@ -1,4 +1,12 @@
 package com.lele.eCommerce.service;
 
+import com.lele.eCommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
 }
