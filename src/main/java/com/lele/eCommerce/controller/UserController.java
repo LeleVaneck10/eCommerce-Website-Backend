92d304c1244a2eeae7eae30e827a45c7485cb8bc
LeleVaneck10 <leletokamvaneckauriol@gmail.com>
2022-06-01 12:05:58 +0100

    Succeded signup

--- src/main/java/com/lele/eCommerce/controller/UserController.java
+++ src/main/java/com/lele/eCommerce/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.lele.eCommerce.controller;
 
+import com.lele.eCommerce.dto.user.ResponseDto;
+import com.lele.eCommerce.dto.user.SignupDto;
+import com.lele.eCommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,21 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
-//    @Autowired
-//    UserService userService;
+    @Autowired
+    UserService userService;
 
     // two apis
 
     // signup
 
-//    @PostMapping("/signup")
-//    public ResponseDto signup(@RequestBody SignupDto signupDto) {
-//        return userService.signUp(signupDto);
-//    }
+    @PostMapping("/signup")
+    public ResponseDto signup(@RequestBody SignupDto signupDto) {
+        return userService.signUp(signupDto);
+    }
 
 
     // signin
-//
+
 //    @PostMapping("/signin")
 //    public SignInReponseDto signIn(@RequestBody SignInDto signInDto) {
 //        return userService.signIn(signInDto);
--- src/main/java/com/lele/eCommerce/service/UserService.java
+++ src/main/java/com/lele/eCommerce/service/UserService.java
@@ -1,7 +1,13 @@
 package com.lele.eCommerce.service;
 
+import com.lele.eCommerce.dto.user.ResponseDto;
+import com.lele.eCommerce.dto.user.SignupDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    public ResponseDto signUp(SignupDto signupDto) {
+        ResponseDto responseDto = new ResponseDto("Success", "dummy Response");
+        return responseDto;
+    }
 }
